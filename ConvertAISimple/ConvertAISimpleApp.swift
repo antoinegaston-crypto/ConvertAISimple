54bbbf099257ab5c9dee9643568e026759cbf17a
Toinou <antoine.gaston@icloud.com>
2026-05-07 02:34:13 +0200

    Initial Commit

---
+++ ConvertAISimple/ConvertAISimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertAISimpleApp.swift
+//  ConvertAISimple
+//
+//  Created by GASTON Antoine on 07/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ConvertAISimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
